82f095ceb3ec3a62ab60a285e28d4b12b9d2f090
Aditya Choudhary <78849471+adityaltts@users.noreply.github.com>
2021-03-20 09:25:57 +0530

    project

---
+++ carrental/src/test/java/com/ltts/carrental/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
